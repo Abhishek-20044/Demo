2908248215837ebff445cea9b09c81daaccec670
Abhishek-20044 <abhishekpattar00@gmail.com>
2025-04-23 14:35:38 +0530

    save to master

--- Demo.java
+++ Demo.java
@@ -4,7 +4,11 @@ public static void main(String[] args) {
     int b=10;
     int sum=a+b;
     int sub=a-b;
+    int mul=a*b;
+    int div=a/b;
     System.out.println("sum is: "+sum);
     System.out.println("sub is: "+sub);
+    System.out.println("mul is: "+mul);
+    System.out.println("div is: "+div);
 }
 }
