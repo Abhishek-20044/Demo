839ed70e5b8e94827700e808bd44105af75aeac5
DARSHAN-DEVIL <www.Darshandeepakjanganure1234@gmail.com>
2025-04-23 14:20:42 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo{
+public static void main(String[] args) {
+    int a=20;
+    int b=10;
+    int sum=a+b;
+    int sub=a-b;
+    System.out.println("sum is: "+sum);
+    System.out.println("sub is: "+sub);
+}
+}
